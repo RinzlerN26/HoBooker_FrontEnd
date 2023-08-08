54e5cd760b65b3e4a5fca3f1b0701faaec319f9b
RinzlerN26 <>
2023-08-08 16:31:54 +0530

    first commit

---
+++ src/components/mailList/MailList.jsx
@@ -0,0 +1,11 @@
+import "./mailList.css"
+
+const MailList = () => {
+  return (
+    <div className="mail">
+      <h1 className="mailTitle">Hobooker: A MERN based property booking web app.</h1>
+    </div>
+  )
+}
+
+export default MailList
